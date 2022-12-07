8917e84c3c1fe8a7f00780ee7efeb78494457197
Dev Choganwala <dev@Devs-MacBook-Air.local>
2022-12-07 22:05:09 +0530

    Add text file support

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from profanity_detector import ProfanityDetector
 
 if __name__ == '__main__':
-    sentences = ["Hello profanity profity profityprofanity"]
-    slurs = ["profanity", "profity"]
+
+    #tweets.txt contains tweets seperated by newlines
+    with open("tweets.txt", "r", encoding='UTF-8') as tweet_file:
+        sentences = tweet_file.read().splitlines()
+    
+    #slurs.txt contains slurs seperated by newlines
+    with open("slurs.txt", "r", encoding='UTF-8') as slurs_file:
+        slurs = slurs_file.read().splitlines()
+    
     detector = ProfanityDetector(slurs, sentences)
     print(detector.get_scores())
